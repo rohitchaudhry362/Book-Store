a34a635e6c856053f61a2497054c90305fbadd6e
Rohit Chaudhry <rohitchaudhry362@gmail.com>
2022-02-11 20:00:36 -0500

    set up index.js for backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,3 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+// Enable cors
+app.use(cors());
+
+// Database conenction
+
+const CONNECTION_URL = 'mongodb://localhost/book_store';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.log(error.message));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
